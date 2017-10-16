cdbf96510ee6485b7f5482b167ba8ba449c75b57
Gunturu <lv.naga.a.gunturu@accenture.com>
2017-10-16 15:10:39 +0530

    commiting files

---
+++ app/app.routing.ts
@@ -0,0 +1,16 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
