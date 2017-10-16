a39c519e6e2ed011eae7a4896a2bb78e0ca95779
Gunturu <lv.naga.a.gunturu@accenture.com>
2017-10-16 18:19:08 +0530

    changes done

---
+++ app/services/user.service.ts
@@ -0,0 +1,11 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: Http) { }
+
+    
+}
