115a4c823a4b0a6a949ef2075ef493220a999a3c
nagaaditya <lv.naga.a.gunturu@accenture.com>
2017-10-19 13:11:43 +0530

    changes done

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,6 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 import { User } from '../models/index';
 
 @Injectable()
