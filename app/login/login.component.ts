08af4048517531d023c848ff812cdc4f4e6e1608
Gunturu <lv.naga.a.gunturu@accenture.com>
2017-12-08 15:52:36 +0530

    changesdone

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -28,9 +28,9 @@ export class LoginComponent implements OnInit {
                 .subscribe(
                     data => {
                         this.users=data[0];
-                        if(this.users.username == this.model.username && this.users.password == this.model.password){
+                        if(this.users && this.users.username == this.model.username && this.users.password == this.model.password){
                         localStorage.setItem('UserName',this.model.username);
-                           this.router.navigate(['/article']);
+                        this.router.navigate(['/article']);
                         }else{
                             this.message = "Invalid Username or Password";
                         }
